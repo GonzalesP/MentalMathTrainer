10d39b522eb9ea56d20d6baa5cd226a74af1c783
GonzalesP <GonzalesP@csu.fullerton.edu>
2024-09-26 15:00:03 -0700

    Decided to start experimenting with the random header class. Doing a rock-paper-scissors side quest lol.

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,44 @@
+#include <cctype>
 #include <iostream>
+#include <random>
+#include <string>
+
+std::string stringToLowercase(std::string str) {
+  std::string lowerStr = "";
+  for (int charIndex = 0; charIndex < str.length(); charIndex++) {
+    lowerStr += std::tolower(str[charIndex]);
+  }
+
+  return lowerStr;
+}
+
+int choiceToInt(std::string choice) {
+  // "clean up" word
+  choice = stringToLowercase(choice);
+
+  if (choice == "rock")
+    return 0;
+  else if (choice == "paper")
+    return 1;
+  else if (choice == "scissors")
+    return 2;
+  else
+    return -1;
+}
+
+// int getRandomChoice() {
+//   std::default_random_engine generator;
+//   std::uniform_int_distribution<int> distribution(0,2);
+//   int choice = distribution(generator);
+
+//   return choice;
+// }
 
 int main() {
-  std::cout << "Hello, world!" << std::endl;
+  std::string userInput = "";
+
+  std::cin >> userInput;
+  std::cout << choiceToInt(userInput) << std::endl;
 
   return 0;
 }
