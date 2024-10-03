6a4cfee12dab6d2c981a49d8c3a6694f71501ace
GonzalesP <GonzalesP@csu.fullerton.edu>
2024-10-03 14:40:15 -0700

    Created CustomRangeAddition, which now allows users to pick a range of numbers that they want to practice adding with.

---
+++ CustomRangeAddition.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <random>
+#include <string>
+
+int main() {
+  int minRange = 0, maxRange = 0;
+  std::cout << "Enter the range of numbers you want to practice with:" << std::endl;
+  std::cout << "Smallest value: ";
+  if (std::cin >> minRange) {
+    std::cout << std::endl << "Largest value: ";
+    if (std::cin >> maxRange) {
+      // do nothing (values accepted)
+    }
+    else {
+      std::cout << "Invalid value. Exiting progratm." << std::endl;
+      return -1;
+    }
+  }
+  else {
+    std::cout << "Invalid value. Exiting program." << std::endl;
+    return -1;
+  }
+
+  std::default_random_engine generator;
+  std::uniform_int_distribution<int> distribution(minRange, maxRange);
+  int userAnswer = 0, leftVar = 0, rightVar = 0;
+
+  leftVar = distribution(generator);
+  rightVar = distribution(generator);
+
+  std::cout << "What is " << leftVar << " + " << rightVar << "?" << std::endl;
+  while(std::cin >> userAnswer) {
+    if (userAnswer == leftVar + rightVar) {
+      std::cout << "That is correct!" << std::endl;
+    }
+    else {
+      std::cout << "Incorrect. The answer was " << leftVar + rightVar << "." << std::endl;
+    }
+    
+    leftVar = distribution(generator);
+    rightVar = distribution(generator);
+    std::cout << "What is " << leftVar << " + " << rightVar << "?" << std::endl;
+  }
+
+  std::cout << "Goodbye." << std::endl;
+
+  return 0;
+}
